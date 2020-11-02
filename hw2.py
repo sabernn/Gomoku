a00599768ed974f622a608d69d3637991cdaa463
sabernn <60275211+sabernn@users.noreply.github.com>
2020-11-01 18:58:40 -0600

    Adding some comments

--- hw2.py
+++ hw2.py
@@ -5,6 +5,7 @@
 
 #### Import libraries
 
+from numpy import random
 import torch
 import numpy as np
 from MCTSBase import TreeNode, MCTSBase
@@ -19,20 +20,30 @@ class TreeNodeSaber(TreeNode):
 
     def is_terminal(self):
         # Write some code here!
+        print("This is a terminal node you stupid!")
 
         pass
 
     def value(self):
+        '''
+        :return: the value of the node form the current player's point of view
+        '''
         # Write some code here!
+
 
         pass
 
     def find_action(self):
         # Write some code here!
+        from random import randrange
 
         # You should retern the action "a" which is the position of the 11x11 board here.
+        print(dir(self))
+        a=(randrange(3),randrange(3))
 
-        pass
+        return a
+
+        # pass
 
     def update(self, v):
         # Write some code here!
@@ -50,36 +61,73 @@ class TreeNodeSaber(TreeNode):
 
 class MCTS(MCTSBase):
     """
-    Some comments!
+    This class inherits from MCTBase class and contains the necessary methods for implementing Monte Carlo Tree Search algorithm
+    for Gomoku game.
     """
 
     def __init__(self,game):
+        '''
+        Your subclass's constructor must call super().__init__(game)
+        :param game: the Gomoku game
+        '''
         super().__init__(game)
         # Write some code here!
         print("This is the constructor of MCTS class!")
-        self.game=game
         pass
 
 
     def reset(self):
+        '''
+        Clean up the internal states and make the class ready for a new tree search
+        :return: None
+        '''
         # Write some code here!
         pass
 
 
     def get_visit_count(self,state):
+        '''
+        Obtain number of visits for each valid (state, a) pairs from this state during the search
+        :param state: the state represented by this node
+        :return: a board_size[0] X board_size[1] matrix of visit counts. It should have zero at locations corresponding to invalid moves at this state.
+        '''
 
         # Write some code here!
         pass
 
 
     def get_treenode(self,standardState):
+        '''
+        Find and return the node corresponding to the standardState in the search tree
+        :param standardState: board state
+        :return: tree node
+        '''
         # Write some code here!
-        pass
+
+        temp=TreeNodeSaber()
+
+        return temp
+        # pass
 
 
     def new_tree_node(self,standardState, game_end):
+        '''
+        Create a new tree node for the search
+        :param standardState: board state
+        :param game_end: whether game ends after last move, takes 3 values: None-> game not end; 0 -> game ends with a tie; 1-> player who made the last move win
+        :return: a new tree node
+        '''
         # Write some code here!
         pass
+
+
+
+# Before "def search(...)" in the base class:
+# [[0,0,1],[1,0,0]] => [black,empty,white] -> winning for white-piace player, losing for black-piece player (confusing for NN)
+# Solution:
+# nn([[0,0,1],[1,0,0]]) -> value for white-piece player
+# nn([[1,0,0],[0,0,1]]) -> value for white-piece player
+
 
 
 
